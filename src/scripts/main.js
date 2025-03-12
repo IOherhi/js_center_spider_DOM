0f96ff136e170a108e61744a8b7fa952268cd2f3
Ivan Herhi <ionherhi@gmail.com>
2025-03-12 10:04:56 +0100

    center02

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,17 @@
 'use strict';
 
 const wall = document.body.querySelector('.wall');
+const spider = document.body.querySelector('.spider');
 
-wall.style.cssText = `
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
+const wallWhigth = wall.clientWidth;
+const wallHeigth = wall.clientHeight;
+
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
+
+const heigth = (wallHeigth - spiderHeight) / 2;
+const width = (wallWhigth - spiderWidth) / 2;
+
+spider.style.position = 'absolute';
+spider.style.top = heigth + 'px';
+spider.style.left = width + 'px';
