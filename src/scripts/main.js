2c88aa2efa09a23ba3c1a078f44d2fb4b53a5903
Ivan Herhi <ionherhi@gmail.com>
2025-03-12 09:15:51 +0100

    center01

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const wall = document.body.querySelector('.wall');
+
+wall.style.cssText = `
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
